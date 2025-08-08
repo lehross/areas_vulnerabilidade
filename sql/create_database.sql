3087f5e6b84c32bc2ff0e76661fee61794df261e
Letícia <leticia.rossaneis@gmail.com>
2025-08-07 23:28:12 -0300

    database creation files

---
+++ sql/create_database.sql
@@ -0,0 +1,11 @@
+-- Criar database
+CREATE DATABASE areas_vulnerabilidade;
+
+
+-- Conectar ao banco (terminal
+-- \c areas_vulnerabilidade;
+
+
+-- Ativar extensão PostGIS
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
---
+++ sql/create_schemas.sql
@@ -0,0 +1,4 @@
+-- Criar schemas para cada camada
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
